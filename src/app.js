b705cb9836767c6ca1c6acdba51d7924ad46c29f
Darragh ORiordan <darragh.oriordan@gmail.com>
2017-09-19 22:31:07 +1200

    add some drop downs and a component

--- src/app.js
+++ src/app.js
@@ -1,17 +1,45 @@
+Vue.component('company-selection', {
+    props: {
+
+        companyCollection: {
+            type: Array,
+            required: true
+        }
+    },
+
+    data: function () {
+        return {
+            selected: "selected",
+            rowId: 10
+        }
+    },
+    template: `<select @change="changeItem(rowId, $event)">
+                <option>select item</option>
+                <option :value="item.company.name"  v-for="item in companyCollection">
+                {{item.company.name}}
+                </option>
+                </select>`,
+    methods: {
+        changeItem: function (rowId, event) {
+            this.selected = `${rowId}, ${event.target.value}`
+        }
+    }
+});
+
 new Vue({
     el: '#appContainer',
-
+    template: '<div><button v-on:click="loadData()">load</button><company-selection :company-collection="companyCollection"></company-selection></div>',
     data: {
-        outputString: ''
+        companyCollection: [],
     },
     methods: {
         loadData: function () {
             var ctrl = this;
-            axios.get('/src/data.json', {
+            axios.get('/data.json', {
                     "encodingType": "UTF8"
                 })
                 .then(function (response) {
-                    ctrl.outputString = JSON.stringify(response.data.data);
+                    ctrl.companyCollection = response.data.data;
                 });
         }
     }
