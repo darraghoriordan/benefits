a2b2e9f0467b2b502495cdf15dd1ca152eb4a83e
Darragh ORiordan <darragh.oriordan@gmail.com>
2017-10-08 23:39:49 +1300

    make the kiwisaver dynamic

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-// register the grid component
 Vue.component('comparison-grid', {
     template: '#grid-template',
     methods: {
@@ -194,24 +193,46 @@ Vue.component('calculator-results', {
             totalEarned: 0
         };
     },
-    props: ['salaryValue'],
+    props: ['salaryValue', 'companyData'],
+    methods: {
+        calculateFixedBenefits: function (annualSalary, companyData) {
+            if (!companyData || !companyData.benefits)
+                return 0;
+            //add percentage things 
+            let percentageBenefits = companyData.benefits.filter(n => n.type == "percent");
+            let percentageSum = percentageBenefits.reduce(function (previousValue, currentValue) {
+                return previousValue += ((currentValue.value / 100) * annualSalary)
+
+            }, 0);
+            //get all type: fixedAmount 
+            //divide value by amortisation
+            // sum
+
+            // get all type:daysLeave
+            // (salary * time)/(amortization [if any])  = (salaryValue)/(40*52) *  ((8 * daysLeaveValue) / amortization)
+            // sum
+
+            return percentageSum;
+        }
+    },
     watch: {
         salaryValue: function () {
             let value = this.salaryValue;
 
             this.salaryFuture = [];
-
-            let kiwisaverPercent = 0.03;
+            let numberOfYearsToCalculate = 5;
+        let standardAnnualRaisePercent = .03;
             this.salaryFuture.push({
                 year: 1,
-                value: Math.floor(value + ((kiwisaverPercent * value)))
+                value: value
             })
             //calc a 3% raise each year
-            for (i = 1; i < 10; i++) {
-                let thisYearSalary = Math.floor(value * (Math.pow((1 + (.03 + kiwisaverPercent) / 1), i)))
+            for (i = 1; i < numberOfYearsToCalculate; i++) {
+                let thisYearSalary = Math.floor(value * (Math.pow((1 + (standardAnnualRaisePercent) / 1), i)));
+                let bfSum = this.calculateFixedBenefits(thisYearSalary, this.companyData);
                 this.salaryFuture.push({
                     year: 1 + i,
-                    value: thisYearSalary
+                    value: thisYearSalary + bfSum
                 })
             }
 
